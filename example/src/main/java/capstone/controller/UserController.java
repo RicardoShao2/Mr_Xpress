a8bf14b97e5336ea998ef4769a64b494b5d331b0
Jasper <yzhu7699@uni.sydney.edu.au>
2023-08-19 00:01:42 +1000

    add 3 method in usercontroller

--- example/src/main/java/capstone/controller/UserController.java
+++ example/src/main/java/capstone/controller/UserController.java
@@ -1,10 +1,16 @@
 package capstone.controller;
 
+import capstone.common.R;
+import capstone.entity.Users;
 import capstone.service.UserService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import javax.servlet.http.HttpServletRequest;
 
 
 @Slf4j
@@ -16,8 +22,36 @@ public class UserController {
     @Autowired
     private UserService userService;
 
+    /*
+    *
+    * 登录方法
+    *
+    * */
+    public R<Users> login(HttpServletRequest request, @RequestBody Users user){
+        return null;
+    }
+
+    /*
+    *
+    *
+    * 注册方法上半段
+    *
+    * */
+    @PostMapping
+    public R<String> sign_up_1(@RequestBody Users user){
+        return null;
+    }
 
 
+    /*
+    *
+    * 注册方法下半段
+    *
+    * */
+    @PostMapping
+    public R<String> sign_up_2(@RequestBody Users user){
+        return null;
+    }
 
 
 }
