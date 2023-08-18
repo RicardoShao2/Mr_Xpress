a4484c27e8344707453a6e4e74023106d14443ef
Jasper <yzhu7699@uni.sydney.edu.au>
2023-08-18 23:38:40 +1000

    add userservice

---
+++ example/src/main/java/capstone/service/UserService.java
@@ -0,0 +1,10 @@
+package capstone.service;
+
+import capstone.entity.Users;
+import com.baomidou.mybatisplus.extension.service.IService;
+import org.springframework.stereotype.Service;
+
+
+public interface UserService extends IService<Users> {
+
+}
---
+++ example/src/main/java/capstone/service/impl/UserServiceImpl.java
@@ -0,0 +1,22 @@
+package capstone.service.impl;
+
+
+import capstone.entity.Users;
+import capstone.mapper.UserMapper;
+import capstone.service.UserService;
+import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+
+@Service
+@Slf4j
+public class UserServiceImpl extends ServiceImpl<UserMapper, Users> implements UserService {
+
+
+
+
+
+
+
+
+}
